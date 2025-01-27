50e7ce27fcc39e508d6d06c5e816cb4e21ea0273
Jeko <jeko.erg@gamil.com>
2025-01-28 00:57:45 +0400

    cc

--- index.php
+++ index.php
@@ -12,11 +12,14 @@ use GraphQL\Type\Schema;
 
 require './vendor/autoload.php';
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-header("Access-Control-Max-Age: 86400");
-// http_response_code(204); // No Content
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+    header("Access-Control-Max-Age: 86400");
+    http_response_code(204); // No Content
+    exit();
+}
 
 $schema = new Schema([
     'query' => new ProductQuery(),
