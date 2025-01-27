b6416dc648764d1807c949eb13dcb84a7d648e44
Jeko <jeko.erg@gamil.com>
2025-01-27 20:53:45 +0400

    cc

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ header("Access-Control-Allow-Origin: *");
 
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Headers: *");
 
 $schema = new Schema([
     'query' => new ProductQuery(),
