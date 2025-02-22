2bd7a11c3f7d7fed0c4b273899a81b7631e87c0f
Jeko <jeko.erg@gamil.com>
2025-02-22 15:03:35 +0400

    cc

--- Core/Data/Database.php
+++ Core/Data/Database.php
@@ -7,7 +7,7 @@ class Database
 {
     public static function getConnection(): PDO
     {
-        $dsn = "mysql:host=127.0.0.1;port=3307;dbname=train;charset=utf8";
+        $dsn = "mysql:host=127.0.0.1;port=3307;dbname=foo;charset=utf8";
         $pdo = new PDO($dsn, 'root', '', [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
